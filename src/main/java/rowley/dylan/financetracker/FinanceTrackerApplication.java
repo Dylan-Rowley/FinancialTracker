10df11bc5cfd2a00ebc37d33e6751aab742ad79b
Dylan-Rowley <d.rowley@celonis.com>
2025-03-16 15:47:31 +0100

    Initial commit - initial dependencies & OAS

---
+++ src/main/java/rowley/dylan/financetracker/FinanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package rowley.dylan.financetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTrackerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FinanceTrackerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/rowley/dylan/financetracker/FinanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package rowley.dylan.financetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTrackerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
